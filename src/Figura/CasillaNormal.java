97900a1c26e2f00e44573c2f9463a4bde325676e
PizzaPepper <57971082+PizzaPepper@users.noreply.github.com>
2020-12-09 19:51:28 -0700

    Documentacion

--- src/Figura/CasillaNormal.java
+++ src/Figura/CasillaNormal.java
@@ -10,15 +10,28 @@ import java.awt.Graphics2D;
 import java.awt.Rectangle;
 
 /**
- *
- * @author Jorge Eliu
+ *Clase de una casilla tipo normal
+ * @author Equipo 5
  */
 public class CasillaNormal extends CasillaDibujo {
-
+    /**
+     * Contructor 
+     * @param x Posicion x de la casilla
+     * @param y Posicion y de la casilla
+     * @param ancho Ancho de la casilla
+     * @param alto Alto de la casilla
+     * @param color Color de la casilla
+     * @param g2d Graphics2D del JPanel
+     */
     public CasillaNormal(int x, int y, int ancho, int alto, Color color, Graphics2D g2d) {
         super(x, y, ancho, alto, color, g2d);
     }
-
+    /**
+     * Metodo que dibuja una casilla normal
+     * @param x Posicion x de la casilla
+     * @param y Posicion y de la casilla
+     * @param g2d Graphics2D del JPanel 
+     */
     @Override
     public void dibujar(int x, int y, Graphics2D g2d) {
         g2d.setColor(color);
