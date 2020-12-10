dba313767fe918426df5102258542a9b35d00ff5
CitOrd385 <56812133+CitOrd385@users.noreply.github.com>
2020-12-09 19:21:30 -0800

    Update Jugador.java
    Jugador documentada

--- src/dominio/Jugador.java
+++ src/dominio/Jugador.java
@@ -3,7 +3,7 @@ package dominio;
 import java.awt.Color;
 
 /**
- *
+ * clase que representa al jugador
  * @author Equipo 5
  */
 public class Jugador {
@@ -13,31 +13,32 @@ public class Jugador {
     private int semillas;
     private Ficha[] fichas;
     
+    //Cosntructor que recibe el nombre del jugador y su cantidad de semillas
     public Jugador(String nombreUsuario, int semillas) {
         this.nombreUsuario = nombreUsuario;
         this.semillas = semillas;
     }
-
+//metodo que recibe el nombre del usuario
     public String getNombreUsuario() {
         return nombreUsuario;
     }
-
+//metodo que recibe el nombre del jugador
     public void setNombreUsuario(String nombreUsuario) {
         this.nombreUsuario = nombreUsuario;
     }
-
+// metodo que devuelve la cantidad de semillas 
     public int getSemillas() {
         return semillas;
     }
-
+//Metodo que recibe la cantidad de semillas
     public void setSemillas(int semillas) {
         this.semillas = semillas;
     }
-
+//Metodo que devuelve el arreglo de fichas por jugador
     public Ficha[] getFichas() {
         return fichas;
     }
-
+//Metodo que recibe el arreglo de fichas por jugador
     public void ConfigurarFichas(int cant,Color color) {
         Ficha[] f=new Ficha[cant];
         for (Ficha ficha : f) {
@@ -47,6 +48,7 @@ public class Jugador {
         this.fichas=f;
     }
 
+    //Metodo que devuelve una cadena de texto con los atributos descritos
     @Override
     public String toString() {
         return "Jugador[" + "nombreUsuario:" + nombreUsuario + ", semillas:" + semillas + ", fichas:" + fichas + ']';
