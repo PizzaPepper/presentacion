346659980f13fddc72563ae2469695ef3e4c4751
CitOrd385 <56812133+CitOrd385@users.noreply.github.com>
2020-12-09 19:10:57 -0800

    Update Ficha.java
    Ficha documentada

--- src/dominio/Ficha.java
+++ src/dominio/Ficha.java
@@ -11,30 +11,32 @@ public class Ficha {
     private Jugador jugador;
     private FichaJugador fichaJ;
 
+     //Constructor por defecto
     public Ficha() {
     }
 
+     //constructor que recibe en su parametro el jugador 
     public Ficha(Jugador jugador) {
         this.jugador = jugador;
 
     }
-
+//Metodo que devuelve el jugador
     public Jugador getJugador() {
         return jugador;
     }
-
+//Metodo que recibe el jugador
     public void setJugador(Jugador jugador) {
         this.jugador = jugador;
     }
-
+//Metodo que devuelve la ficha del jugador
     public FichaJugador getFichaJ() {
         return fichaJ;
     }
-
+//metodo que recibe la ficha del jugador
     public void setFichaJ(FichaJugador fichaJ) {
         this.fichaJ = fichaJ;
     }
-
+//Metodo que devuelve una cadena de texto con los atributos correspondientes
     @Override
     public String toString() {
         return "Ficha{" + "jugador=" + jugador + ", fichaJ=" + fichaJ + '}';
