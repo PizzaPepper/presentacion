182e4ef8f30dff074c0b551131d8484bd97b5390
PizzaPepper <57971082+PizzaPepper@users.noreply.github.com>
2020-12-09 20:07:33 -0700

    Documentacion

--- src/Figura/CasillaTriangulo.java
+++ src/Figura/CasillaTriangulo.java
@@ -13,26 +13,47 @@ import java.awt.Polygon;
 import java.awt.Rectangle;
 
 /**
- *
- * @author Jorge Eliu
+ * Clase de una casilla tipo triangulo
+ * @author Equipo5
  */
 public class CasillaTriangulo extends CasillaDibujo{
     
     private Sentido sentido;
-
+    /**
+     * Contructor 
+     * @param x Posicion x de la casilla
+     * @param y Posicion y de la casilla
+     * @param ancho Ancho de la casilla
+     * @param alto Alto de la casilla
+     * @param color Color de la casilla
+     * @param sentido Sentido que apunta el triangulo
+     * @param g2d Graphics2D del JPanel
+     */
     public CasillaTriangulo( int x, int y, int ancho, int alto, Color color, Sentido sentido,Graphics2D g2d) {
         super(x, y, ancho, alto, color, g2d);
         this.sentido = sentido;
     }
+    /**
+     * Metodo que regresa el sentido
+     * @return sentido
+     */
     public Sentido getSentido() {
         return sentido;
     }
-
+    /**
+     * Metodo que establece el sentido
+     * @param sentido  
+     */
     public void setSentido(Sentido sentido) {
         this.sentido = sentido;
     }
     
-
+    /**
+     * Metodo que dibuja una casilla triangular
+     * @param x Posicion x de la casilla
+     * @param y Posicion y de la casilla
+     * @param g2d Graphics2D del JPanel 
+     */
     @Override
     public void dibujar(int x, int y, Graphics2D g2d) {
         Polygon triangulo;
