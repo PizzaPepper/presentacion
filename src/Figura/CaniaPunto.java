b0e9ba93617cf0b9134807e91b80e07cbc6163d6
PizzaPepper <57971082+PizzaPepper@users.noreply.github.com>
2020-12-09 20:19:53 -0700

    Documentacion

--- src/Figura/CaniaPunto.java
+++ src/Figura/CaniaPunto.java
@@ -21,7 +21,14 @@ public class CaniaPunto implements IFigura{
     private int ancho;
     private int alto;
     private Graphics2D g2d;
-
+    /**
+     * Contructor 
+     * @param x Posicion x de la casilla
+     * @param y Posicion y de la casilla
+     * @param ancho Ancho de la casilla
+     * @param alto Alto de la casilla
+     * @param g2d Graphics2D del JPanel
+     */
     public CaniaPunto(int x, int y, int ancho, int alto, Graphics2D g2d) {
         this.x = x;
         this.y = y;
@@ -29,48 +36,85 @@ public class CaniaPunto implements IFigura{
         this.alto = alto;
         this.g2d = g2d;
     }
-
+    
+     /**
+     * Metodo que Regresa la posicion X
+     * @return X
+     */
     public int getX() {
         return x;
     }
-
+    /**
+     * Metodo que establece el valor de la posicion X
+     * @param x Valor a cambiar
+     */
     public void setX(int x) {
         this.x = x;
     }
-
+    /**
+     * Metodo que Regresa la posicion Y
+     * @return y
+     */
     public int getY() {
         return y;
     }
-
+    
+    /**
+     * Metodo que establece el valor de la posicion Y
+     * @param y Valor a cambiar
+     */
     public void setY(int y) {
         this.y = y;
     }
-
+    /**
+     * Metodo que Regresa Ancho
+     * @return ancho
+     */
     public int getAncho() {
         return ancho;
     }
-
+    /**
+     * Metodo que establece el ancho
+     * @param ancho Valor a cambiar
+     */
     public void setAncho(int ancho) {
         this.ancho = ancho;
     }
-
+    /**
+     * Metodo que regresa Ancho
+     * @return Alto
+     */
     public int getAlto() {
         return alto;
     }
-
+    /**
+     * Metodo que establece el alto
+     * @param Alto Valor a cambiar
+     */
     public void setAlto(int alto) {
         this.alto = alto;
     }
-
+    
+    /**
+     * Metodo que regresa Graphics2D
+     * @return g2d
+     */
     public Graphics2D getG2d() {
         return g2d;
     }
-
+    /**
+     * Metodo que establece el Graphics2D
+     * @param g2d Valor a cambiar
+     */
     public void setG2d(Graphics2D g2d) {
         this.g2d = g2d;
     }
-    
-    
+    /**
+     * Metodo que dibuja una cania punto
+     * @param x Posicion x de la casilla
+     * @param y Posicion y de la casilla
+     * @param g2d Graphics2D del JPanel 
+     */
     @Override
     public void dibujar(int x, int y, Graphics2D g2d) {
         
