31f60dbbf9d73e3c9f17e1f7442217ef39ee4703
CitOrd385 <56812133+CitOrd385@users.noreply.github.com>
2020-12-09 19:08:38 -0800

    Update Casilla.java
    Casilla documentada

--- src/dominio/Casilla.java
+++ src/dominio/Casilla.java
@@ -1,7 +1,7 @@
 package dominio;
 
 /**
- *
+ * Clase padre que va a heredar los metodos correspondientes a sus casillas hijas
  * @author Equipo 5
  */
 public abstract class Casilla {
@@ -9,24 +9,27 @@ public abstract class Casilla {
     private FichaJugador fichaActual;
     private Posicion pos;
 
+     //Constructor que inicializa los atributos ficha y posición
     public Casilla() {
         this.fichaActual=null;
         this.pos=null;
     }
 
+     //Metodo get de ficha actual que devuelve lo antes dicho
     public FichaJugador getFichaActual() {
         return fichaActual;
     }
 
+     //Metodo que recibe la ficha 
     public void setFichaActual(FichaJugador fichaActual) {
         this.fichaActual = fichaActual;
     }
 
-
+//metodo que devuelve la posición ficha
     public Posicion getPos() {
         return pos;
     }
-
+//metodo que recibe la posición
     public void setPos(Posicion pos) {
         this.pos = pos;
     }
